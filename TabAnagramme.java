fe7b413a4879c0fc02d88809eec92ba143b909e1
PyuK <ucanttoucheme@gg.com>
2015-03-18 21:57:30 +0100

    TP
    Anagramme

---
+++ TabAnagramme.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+ 
+ public class TabAnagramme{
+	 private ArrayList <String> tab;
+	 
+	 public TabAnagramme(){
+		 tab=new ArrayList();
+	 }
+	 
+	 public ArrayList<String> gettab(){
+		 return tab;
+	 }
+	 
+	 public void anagrammes(char [] mot, int indice){
+		 if(mot.length <= indice){
+			 
+		 }
+		 
+		 else{
+			 
+			 
+		 }
+		 
+		 
+	 }	 
+	 
+	 
+	 
+	 //main
+	 public static void main(String [] args){
+		char [] mot={'a','b','c'};
+		 for(int i=0;i<=2;i++){
+			 System.out.print(mot[i]);
+		 }
+	 }
+ }
